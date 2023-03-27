3b1f60a6f44feec09b89c452ca4be0b369ac37fe
MariaHeloisa300512 <119361001+MariaHeloisa300512@users.noreply.github.com>
2023-03-27 17:52:15 -0300

    Add files via upload

---
+++ Kwitter-Maria-Heloisa--main/Kwitter_completo Maria Heloisa/Kwitter_completo-main/web_page.js
@@ -0,0 +1,13 @@
+//LINKS DO SEU APP FIREBASE
+const firebaseConfig = {
+  apiKey: "AIzaSyCG8j5K3DiMASeIxDsP_05gIP6Y1Fmm1Zw",
+  authDomain: "kwiitter-c0c6b.firebaseapp.com",
+  databaseURL: "https://kwiitter-c0c6b-default-rtdb.firebaseio.com",
+  projectId: "kwiitter-c0c6b",
+  storageBucket: "kwiitter-c0c6b.appspot.com",
+  messagingSenderId: "362899324362",
+  appId: "1:362899324362:web:d57ee6ca8b436b0da4a333"
+};
+
+//inicializar o Firebase
+firebase.initializeApp(firebaseConfig);
